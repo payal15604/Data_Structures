d68338efd8cc68313a4bae8e997fbd1ccad32fdc
Payal <ppayal_be22@thapar.edu>
2024-07-06 00:10:46 +0530

    medium

---
+++ Arrays/Sliding Window/Binary Subarrays With Sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int goal) {
+        if(goal < 0) return 0;
+
+        long l = 0, r = 0, sum = 0, count = 0;
+        while(r < nums.size())
+        {
+            sum += nums[r];
+
+            while(sum > goal)
+            {
+                sum -= nums[l];
+                l++;
+            }
+
+            count = count + (r-l+1);
+            r++;
+        }
+        return count;
+    }
+
+    int numSubarraysWithSum(vector<int>& nums, int goal)
+{
+    return solve(nums, goal) - solve(nums, goal-1);
+}
+};
+
