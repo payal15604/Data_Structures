c5209d16ed880e149322bdf325464f53a4f2c51d
Payal <ppayal_be22@thapar.edu>
2024-07-12 15:29:50 +0530

    med

---
+++ Arrays/Sorting/Sort Colors.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int one = 0, two = 0, zero = 0;
+        int n = nums.size();
+        
+
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 0) {zero++;}
+            else if(nums[i] == 1) {one++;}
+            else {two++;}
+        }
+
+        for(int i = 0; i < zero; i++)
+        {
+            nums[i] = 0;
+        }
+
+        for(int i = zero; i < zero + one; i++)
+        {
+            nums[i] = 1;
+        }
+
+        for (int i = zero + one; i < n; i++)
+        {
+            nums[i] = 2;
+        }
+
+    
+    }
+};
