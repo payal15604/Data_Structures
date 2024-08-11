f8cd5581ecf63cf90ad054cfca265bcd25f5b92e
Payal <ppayal_be22@thapar.edu>
2024-08-11 17:09:15 +0530

    medium

---
+++ Arrays/Hashing/Group Anagrams.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(auto x : strs)
+        {
+            string tbs = x;
+            sort(tbs.begin(), tbs.end());
+            mp[tbs].emplace_back(x);
+
+        }
+
+        vector<vector<string>> ans;
+        for(auto x : mp)
+        {
+            ans.emplace_back(x.second);
+        }
+
+        return ans;
+    }
+};
+
+/*class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+        for (int i = 0; i < strs.size(); i++) {
+            string key = getKey(strs[i]);
+            m[key].push_back(strs[i]);
+        }
+        
+        vector<vector<string>> result;
+        for (auto it = m.begin(); it != m.end(); it++) {
+            result.push_back(it->second);
+        }
+        return result;
+    }
+
+    string getKey(string str) {
+        vector<int> count(26);
+        for (int j = 0; j < str.size(); j++) {
+            count[str[j] - 'a']++;
+        }
+        
+        string key = "";
+        for (int i = 0; i < count.size(); i++) {
+            key.append(to_string(count[i]) + '#');
+        }
+        return key;
+    }
+};
+*/
+
