26287f4b97799ad1a743e4d81d3f08363394fdb9
Payal <ppayal_be22@thapar.edu>
2024-08-25 13:26:43 +0530

    easy

---
+++ Linked List/Linked List Cycle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if (head == NULL) {
+            return false;
+        }
+        unordered_map<ListNode*, bool> visited;
+        ListNode* temp = head;
+
+        while (temp != NULL) {
+            if (visited[temp] == true) {
+                return true;
+            }
+            visited[temp] = true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
