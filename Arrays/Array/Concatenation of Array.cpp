7bc9f30d634720c4e89ebe664bc350088e9d44b7
Payal <ppayal_be22@thapar.edu>
2025-01-02 18:16:53 +0530

    easy

---
+++ Arrays/Array/Concatenation of Array.cpp
@@ -0,0 +1,25 @@
+// class Solution {
+// public:
+//     vector<int> getConcatenation(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n*2, 0);
+//         for(int i = 0; i < n; i++)
+//         {
+//             ans[i] = nums[i];
+//             ans[i+n] = nums[i];
+//         }
+
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            nums.emplace_back(nums[i]);
+        }
+        return nums;
+    }
+};
