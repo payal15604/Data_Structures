ef58e86bba25b9e50ab1b31ea113b0cdc4a6d8aa
Payal <ppayal_be22@thapar.edu>
2025-01-22 23:01:25 +0530

    medium TLE

--- Arrays/Sets/Longest Consecutive Sequence.cpp
+++ Arrays/Sets/Longest Consecutive Sequence.cpp
@@ -20,3 +20,44 @@ public:
         return longest;
     }
 };
+
+//TLE giving with new approach
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, bool> map;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]] = true;
+        }
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(map.count(nums[i]-1)>0)
+            {
+                map[nums[i]] = false;
+            }
+        }
+
+        int maxlen = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(map[nums[i]] == true){
+                int j = 0; int count = 0;
+                while(map.count(nums[i] + j) > 0)
+                {
+                    j++;
+                    count++;
+                } 
+
+                maxlen = max(maxlen, count);
+            }
+        }
+
+         return maxlen;
+    }
+
+  
+};
