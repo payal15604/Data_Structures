3eb8560dbeafc9b97e943463fcc0437e6ec2ed59
Payal <ppayal_be22@thapar.edu>
2025-01-25 10:43:27 +0530

    easy

--- Binary Tree/Maximum Depth of Binary Tree.cpp
+++ Binary Tree/Maximum Depth of Binary Tree.cpp
@@ -18,3 +18,47 @@ public:
         return 1 + max(maxDepth(root->left),maxDepth(root->right));
     }
 };
+
+//iterative approach
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        stack<pair<TreeNode*, int>> st;
+        st.push({root, 1});
+        int maxDepth = 1;
+
+        if(root == nullptr) return 0;
+
+        while(!st.empty())
+        {
+            auto [curr, depth] = st.top();
+            st.pop();
+
+            if(curr->left != nullptr)
+            {
+                st.push({curr->left, depth + 1});
+            }
+
+            if(curr->right != nullptr)
+            {
+                st.push({curr->right, depth +1});
+            }
+
+            maxDepth = max(maxDepth, depth);
+        }
+        
+        return maxDepth;
+    }
+};
